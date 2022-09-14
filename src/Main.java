1ac7f55ff8eaab277289974490d9993d25ae6515
679 <apgettman@gmail.com>
2022-09-15 00:03:19 +0300

    ДЗ-8 Циклы.Урок-2,

--- src/Main.java
+++ src/Main.java
@@ -36,59 +36,55 @@ public class Main {
         // Урок 2 Задание 1,2
         int vasya = 15000;
         int vasyaBank = 1;
-        int iN = 1;
-        while (iN <= 12) {
-            while (vasyaBank < 12_000_000) {
-                vasyaBank += vasyaBank / 7;
-                vasyaBank = vasya + vasyaBank;
-                if (iN % 6 == 0) {  // Урок 2 Задание 2
-                    System.out.println("Месяц " + iN + ", сумма накоплений равна " + vasyaBank + " рублей!");
-                }
-                iN++;
-            } // Урок 2 Задание 3
-            int vasyaA = 15000;
-            int total = 0;
-            int iW = 1;
-            while (iW < 9 * 12) {
-                total = total + total / 100 * 7;
-                total = total + vasyaA;
-                if (iW % 6 == 0) {
-                    System.out.println("Месяц " + iW + ", сумма накоплений равна " + total + " рублей!");
-                }
-                iW++;
+        while (vasyaBank < 12_000_000) {
+            vasyaBank += vasyaBank / 7;
+            vasyaBank = vasya + vasyaBank;
+            if (i % 6 == 0) {  // Урок 2 Задание 2
+                System.out.println("Месяц " + i + ", сумма накоплений равна " + vasyaBank + " рублей!");
             }
-            // Урок 2 Задание 4
-            int friday = 2;
-            int iF = 1;
-            while (iF < 31) {
-                if (iF % 7 == 2) {
-                    System.out.println("Сегодня пятница, " + iF + "-е число. Необходимо подготовить отчет.");
-                }
-                iF++;
+            i++;
+        } // Урок 2 Задание 3
+        int vasyaA = 15000;
+        int total = 0;
+        while (i < 9 * 12) {
+            total = total + total / 100 * 7;
+            total = total + vasyaA;
+            if (i % 6 == 0) {
+                System.out.println("Месяц " + i + ", сумма накоплений равна " + total + " рублей!");
             }
-            // Урок 3 Задание 1
-            int earthA = 2122;
-            int yearsY = 1822;
-            for (int iQ = 0; iQ <= earthA; iQ = iQ + 79) {
-                if (iQ >= yearsY) {
-                    System.out.println(iQ);
-                }
-            }
-            // Урок 3 Задание 2
-            int table1 = 0;
-            int table2 = 2;
-            System.out.print(table1 + " ");
-            System.out.print(table2 + " ");
-
-            int iR = 0;
-            do {
-                int tmp = table2;
-                table2 = table1 + table2;
-                table1 = tmp;
-                iR++;
-                System.out.print(table2 + " ");
-            }
-            while (iR != 8);
+            i++;
         }
+        // Урок 2 Задание 4
+        int friday = 2;
+        int iF = 1;
+        while (iF < 31) {
+            if (iF % 7 == 2) {
+                System.out.println("Сегодня пятница, " + iF + "-е число. Необходимо подготовить отчет.");
+            }
+            iF++;
+        }
+        // Урок 3 Задание 1
+        int earthA = 2122;
+        int yearsY = 1822;
+        for (int iQ = 0; iQ <= earthA; iQ = iQ + 79) {
+            if (iQ >= yearsY) {
+                System.out.println(iQ);
+            }
+        }
+        // Урок 3 Задание 2
+        int table1 = 0;
+        int table2 = 2;
+        System.out.print(table1 + " ");
+        System.out.print(table2 + " ");
+
+        int iR = 0;
+        do {
+            int tmp = table2;
+            table2 = table1 + table2;
+            table1 = tmp;
+            iR++;
+            System.out.print(table2 + " ");
+        }
+        while (iR != 8);
     }
 }
